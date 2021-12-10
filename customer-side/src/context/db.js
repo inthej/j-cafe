3888674725f549767d83e41cc4f279c8b1a8c335
inthej <inthej12@gmail.com>
2021-12-11 01:08:39 +0900

    Delete dummy data

--- customer-side/src/context/db.js
+++ customer-side/src/context/db.js
@@ -1,13 +1,17 @@
 import {createContext} from "react";
-import {SizeType} from "../common/AppTypes";
 
+/*
+ data sample
+ [
+  {id: 0, sizeType: SizeType.small, title: 'Latte', amount: 1, price: 4},
+  {id: 1, sizeType: SizeType.medium, title: 'Matcha', amount: 1, price: 4.5},
+  {id: 2, sizeType: SizeType.large, title: 'Iced Latte', amount: 1, price: 5.5},
+  {id: 3, sizeType: SizeType.small, title: 'Cappuccino', amount: 1, price: 4}
+  ]
+ * @type {React.Context<{dbMyFavouriteList: *[], dbMenuList: [{path: string, price: {small: number, large: number, medium: number}, id: number, title: string}, {path: string, price: {small: number, large: number, medium: number}, id: number, title: string}, {path: string, price: {small: number, large: number, medium: number}, id: number, title: string}, {path: string, price: {small: number, large: number, medium: number}, id: number, title: string}]}>}
+ */
 const DbContext = createContext({
-  dbMyFavouriteList: [
-    {id: 0, sizeType: SizeType.small, title: 'Latte', amount: 1, price: 4},
-    // {id: 1, sizeType: SizeType.medium, title: 'Matcha', amount: 1, price: 4.5},
-    // {id: 2, sizeType: SizeType.large, title: 'Iced Latte', amount: 1, price: 5.5},
-    // {id: 3, sizeType: SizeType.small, title: 'Cappuccino', amount: 1, price: 4},
-  ],
+  dbMyFavouriteList: [],
 
   dbMenuList: [
     {id: 0, title: 'Latte', path: '/images/latte.png', price: {small: 4, medium: 4.5, large: 5.5}},
